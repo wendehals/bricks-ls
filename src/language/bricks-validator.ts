1b9efa04f414643a8a354ae5f38991a3c1437e3f
Lothar Wendehals <lothar.wendehals@itemis.de>
2023-07-07 15:37:57 +0200

    Added unique names check, fixed packaging

--- src/language/bricks-validator.ts
+++ src/language/bricks-validator.ts
@@ -1,5 +1,5 @@
 import { ValidationAcceptor, ValidationChecks } from 'langium';
-import { BricksAstType, Build } from './generated/ast';
+import { BricksAstType, Build, Model } from './generated/ast';
 import type { BricksServices } from './bricks-module';
 
 /**
@@ -10,6 +10,7 @@ export function registerValidationChecks(services: BricksServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.BricksValidator;
     const checks: ValidationChecks<BricksAstType> = {
+        Model: validator.checkIdentity,
         Build: validator.checkBuildMode
     };
     registry.register(checks, validator);
@@ -21,6 +22,19 @@ const buildModeRegex : RegExp = /^[campCAMP]+$/;
  * Implementation of custom validations.
  */
 export class BricksValidator {
+
+    checkIdentity(model: Model, accept: ValidationAcceptor): void {
+        let ids = new Set<string>()
+        for (var cmd of model.commands){
+            if ("name" in cmd) {
+                if (ids.has(cmd.name)) {
+                    accept('error', 'The variable name is not unique.', { node: cmd, property: 'name' });    
+                } else {
+                    ids.add(cmd.name)
+                }
+            }
+        }
+    }
 
     checkBuildMode(build: Build, accept: ValidationAcceptor): void {
         if (build.buildMode) {
